d40431ca4ff71ce89daeb64ff85311faa911e63f
张乐 <leroyzhang66@gmai.com>
2018-07-01 23:01:50 +0800

    动态方法调用-----dynamic---DynamicDemo1_*

---
+++ src/cn/itheima/b_dynamic/DynamicDemo1.java
@@ -0,0 +1,25 @@
+package cn.itheima.b_dynamic;
+
+public class DynamicDemo1 {
+
+	public String add() {
+		System.out.println("添加方法!");
+		return "success";
+	}
+	
+	public String delete() {
+		System.out.println("删除方法!");
+		return "success";
+	}
+	
+	public String update() {
+		System.out.println("修改方法!");
+		return "success";
+	}
+	
+	public String find() {
+		System.out.println("查找方法!");
+		return "success";
+	}
+	
+}
